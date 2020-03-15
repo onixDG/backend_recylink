3a667408eb79829f8e5d0ab822856425fceda6aa
Daniela Milena Guerrero Pérez <onix.dg@MacBook-Pro-de-Daniela.local>
2020-03-15 02:20:46 -0300

    Created app and database conection

--- src/database.js
+++ src/database.js
@@ -1,2 +1,14 @@
-const express =  require('express');
-const app = express();
+const mongoose = require('mongoose');
+
+const URI = process.env.DATABASE_URI;
+
+mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+});
+
+const connection = mongoose.connection;
+
+connection.once('open',() => {
+    console.log('DB is connected')
+});
